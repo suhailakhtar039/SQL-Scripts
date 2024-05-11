711ec526e7c7f37a739116cd0d03fe0779053739
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-11 23:52:17 +0530

    inserting multiple values

--- InsertValues.sql
+++ InsertValues.sql
@@ -4,10 +4,21 @@ CREATE TABLE cats (
 );
 -- INSERTING VALUES
 INSERT INTO cats(name, age)
-VALUES('PIXIE',4);
-
-INSERT INTO cats(name, age)
-VALUES('sunday',5);
+VALUES	('PIXIE',4),
+		('sunday',5);
 
 select * from cats;
 
+-- Multi insert
+CREATE TABLE people(
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+    age int
+);
+
+INSERT INTO people	(first_name, last_name, age)
+VALUES				("Linda", "Belcher", 45),
+					("Philip", "Frond", 38),
+                    ("Calvin", "Klein", 70);
+
+SELECT * FROM people;
