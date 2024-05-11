adac12ab82325f98e4c1e76ca25bf52382ff2425
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-11 16:39:52 +0530

    table created

---
+++ TablesInformation.sql
@@ -0,0 +1,15 @@
+-- create tables
+
+-- CREATE TABLE example(
+-- column_value dataType,
+-- column_value, dataType
+-- )
+
+CREATE DATABASE pet_shop;
+
+use pet_shop;
+
+CREATE TABLE cats(
+name varchar(50),
+age int
+)
