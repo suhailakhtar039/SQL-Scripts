7550bde68a4874dcdb91c55bd501bb7a72e36f4e
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-12 16:36:47 +0530

    using default values

--- Table/cats3.sql
+++ Table/cats3.sql
@@ -2,3 +2,14 @@ CREATE TABLE cats3(
 	name VARCHAR(100) DEFAULT 'unnamed',
     age INT DEFAULT 99
 );
+
+-- see the content
+
+SELECT * FROM cats3;
+
+INSERT INTO cats3()
+VALUES();
+
+INSERT INTO cats3(age)
+VALUES(45);
+
