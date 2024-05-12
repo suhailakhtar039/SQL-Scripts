de01049029a99a6682728892a7b10a2f933ffbca
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-12 16:16:33 +0530

    described

--- OperationOnCats.sql
+++ OperationOnCats.sql
@@ -2,3 +2,19 @@ SELECT * FROM pet_shop.cats;
 INSERT INTO cats(name)
 VALUES("random");
 -- that means we can include null values
+
+-- creating 2nd table
+CREATE TABLE cats2(
+	name VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+);
+
+INSERT INTO cats2(name, age)
+VALUES("random",4);
+
+SELECT * FROM cats2;
+
+INSERT INTO cats2(name)
+VALUES ("abc");
+
+DESC cats2;
