7ede68e2944c38ab8f456ce966cf14f545556db2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-12 17:21:23 +0530

    setting primary key

--- Table/cats3.sql
+++ Table/cats3.sql
@@ -13,3 +13,28 @@ VALUES();
 INSERT INTO cats3(age)
 VALUES(45);
 
+INSERT INTO cats3(name, age)
+VALUES(NULL, NULL);
+
+INSERT INTO cats3(name, age)
+VALUES('abc', 4);
+
+-- start with keys
+CREATE TABLE unique_cats(
+	cat_id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+
+SELECT * FROM unique_cats;
+
+DESC unique_cats;
+
+INSERT INTO unique_cats(cat_id, name, age)
+VALUES(2, 'random', 8);
+
+INSERT INTO unique_cats(cat_id, name, age)
+VALUES(5, 'pixie', 18);
+
+INSERT INTO unique_cats(cat_id, name, age)
+VALUES(5, 'pixie', 18);
