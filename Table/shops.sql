b6ec652a6fc45723ff88e2d8a2a4ca3a5d5767e6
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-12 16:27:24 +0530

    how to use two quotes in data

--- Table/shops.sql
+++ Table/shops.sql
@@ -2,5 +2,11 @@ CREATE TABLE shops(
 	name VARCHAR(100)
 );
 
+-- see the content
+SELECT * FROM shops;
+
 INSERT INTO shops(name)
 VALUES('shoe emporium');
+
+INSERT INTO shops(name)
+VALUES('mario\'s clothes');
