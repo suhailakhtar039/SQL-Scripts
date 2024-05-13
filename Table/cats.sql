ce81c0f7b0c39efdff8d6692fcea0a2467c9a216
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-13 11:53:13 +0530

    where clause used along with rapid fire series

--- Table/cats.sql
+++ Table/cats.sql
@@ -13,23 +13,37 @@ CREATE TABLE cats
     PRIMARY KEY
     (cat_id)
 );
-    SELECT *
-    FROM cats;
-    DESC cats;
-    INSERT INTO cats
-        (name, breed, age)
-    VALUES
-        ('Ringo', 'Tabby', 4),
-        ('Cindy', 'Maine Coon', 10),
-        ('Dumbledore', 'Maine Coon', 11),
-        ('Egg', 'Persian', 4),
-        ('Misty', 'Tabby', 13),
-        ('George Michael', 'Ragdoll', 9),
-        ('Jackson', 'Sphynx', 7);
+SELECT *
+FROM cats;
+DESC cats;
+INSERT INTO cats
+	(name, breed, age)
+VALUES
+	('Ringo', 'Tabby', 4),
+	('Cindy', 'Maine Coon', 10),
+	('Dumbledore', 'Maine Coon', 11),
+	('Egg', 'Persian', 4),
+	('Misty', 'Tabby', 13),
+	('George Michael', 'Ragdoll', 9),
+	('Jackson', 'Sphynx', 7);
 
 
-    -- selecting differenct columns
-    SELECT name, age
-    FROM cats;
+-- selecting differenct columns
+SELECT name, age
+FROM cats;
 
+SELECT name, breed FROM cats;
+    
+-- using where clause
 
+SELECT * FROM cats WHERE age = 4;
+
+SELECT name, age WHERE age = 4;
+    
+-- rapid fire series
+
+SELECT cat_id FROM cats;
+
+SELECT name, breed FROM cats;
+
+SELECT cat_id, age FROM cats WHERE cat_id = age;
