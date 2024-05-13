b1ac9ae9cfc54a7b8319c8bfd65a3689c826cc46
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-13 23:22:09 +0530

    getting full name

---
+++ Table/stringFunctions.sql
@@ -0,0 +1,11 @@
+SELECT CONCAT('H', 'e', 'g', 'o');
+-- concat in string
+SELECT CONCAT(author_fname,'!!!') FROM books;
+
+-- concat for full name
+SELECT CONCAT(author_fname, ' ', author_lname) as full_name FROM books;
+
+
+
+
+
