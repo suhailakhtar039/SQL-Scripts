9ce26762bf354189857ee287666b3df9519f8f3c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-13 11:36:33 +0530

    seeing different types of select

--- Table/cats.sql
+++ Table/cats.sql
@@ -1,26 +1,35 @@
 -- drop table cats
 DROP TABLE cats;
 -- create cats
-CREATE TABLE cats(
-	cat_id INT AUTO_INCREMENT,
-    name VARCHAR(100),
-    breed VARCHAR(100),
+CREATE TABLE cats
+(
+    cat_id INT
+    AUTO_INCREMENT,
+    name VARCHAR
+    (100),
+    breed VARCHAR
+    (100),
     age INT,
-    PRIMARY KEY(cat_id)
+    PRIMARY KEY
+    (cat_id)
 );
-SELECT * FROM cats;
-DESC cats;
-INSERT INTO cats(name, breed, age) 
-VALUES ('Ringo', 'Tabby', 4),
-       ('Cindy', 'Maine Coon', 10),
-       ('Dumbledore', 'Maine Coon', 11),
-       ('Egg', 'Persian', 4),
-       ('Misty', 'Tabby', 13),
-       ('George Michael', 'Ragdoll', 9),
-       ('Jackson', 'Sphynx', 7);
+    SELECT *
+    FROM cats;
+    DESC cats;
+    INSERT INTO cats
+        (name, breed, age)
+    VALUES
+        ('Ringo', 'Tabby', 4),
+        ('Cindy', 'Maine Coon', 10),
+        ('Dumbledore', 'Maine Coon', 11),
+        ('Egg', 'Persian', 4),
+        ('Misty', 'Tabby', 13),
+        ('George Michael', 'Ragdoll', 9),
+        ('Jackson', 'Sphynx', 7);
 
 
--- selecting differenct columns
-SELECT name, age FROM cats;
+    -- selecting differenct columns
+    SELECT name, age
+    FROM cats;
 
 
