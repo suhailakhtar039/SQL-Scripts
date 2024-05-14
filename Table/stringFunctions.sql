dfee32174e9fec8c22c147a5e3be449b62303a47
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-14 23:31:28 +0530

    trim anything including spaces as well

--- Table/stringFunctions.sql
+++ Table/stringFunctions.sql
@@ -83,6 +83,17 @@ SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
  
 SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
 
+-- trim
+SELECT INSERT('Hello Bobby', 6, 0, 'There'); 
+ 
+SELECT LEFT('omghahalol!', 3);
+ 
+SELECT RIGHT('omghahalol!', 4);
+ 
+SELECT REPEAT('ha', 4);
+ 
+SELECT TRIM('  pickle  ');
+
 
 
 
