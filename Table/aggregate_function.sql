89a576b84efdc3db733919fb981985cbf0c2ac4a
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-25 16:34:45 +0530

    using distinct author first name

--- Table/aggregate_function.sql
+++ Table/aggregate_function.sql
@@ -5,5 +5,13 @@ FROM books;
 -- counting authors first name
 -- this will simply tell you how many times author_fname occured
 -- It won't tell you distinct value just value
-SELECT COUNT(author_fname) AS no_of_authors
-FROM books;
+SELECT 
+    COUNT(author_fname) AS no_of_authors
+FROM
+    books;
+    
+-- this one is for distinct previous was for just author
+SELECT 
+    COUNT(DISTINCT author_fname) AS no_of_authors
+FROM
+    books;
