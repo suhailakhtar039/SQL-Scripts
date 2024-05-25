0909b804b4ee96d6397bfad5b87c0ac6a3d0e4b4
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-25 16:30:14 +0530

    using count for other field

--- Table/aggregate_function.sql
+++ Table/aggregate_function.sql
@@ -1,2 +1,9 @@
 -- aggregate function started
-SELECT COUNT(*) FROM books;
+SELECT COUNT(*)
+FROM books;
+
+-- counting authors first name
+-- this will simply tell you how many times author_fname occured
+-- It won't tell you distinct value just value
+SELECT COUNT(author_fname) AS no_of_authors
+FROM books;
