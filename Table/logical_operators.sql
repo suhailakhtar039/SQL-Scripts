c3f4701e80bb3d97d84277a37ec44787ddf6814f
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-28 16:05:27 +0530

    less than greater than operator ended

--- Table/logical_operators.sql
+++ Table/logical_operators.sql
@@ -8,11 +8,17 @@ SELECT title FROM books WHERE title NOT LIKE '% %';
 SELECT title FROM books WHERE title LIKE '%e%';
 SELECT title FROM books WHERE title NOT LIKE '%e%';
 
--- using greater than and smaller than operator
+-- using greater than 
 SELECT * FROM books WHERE released_year > 2010;
 SELECT * FROM books WHERE pages > 500;
 SELECT 1 > NULL;
 SELECT NULL > 1;
 
+-- using smaller than operator
+SELECT * FROM books WHERE released_year < 2000;
+
+SELECT * FROM books WHERE released_year >= 2010;
+
+SELECT * FROM books WHERE released_year <= 1985;
 
 
