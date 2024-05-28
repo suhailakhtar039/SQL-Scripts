7c4e3e78c7cb3f6da7fe9e73233f7f5dc4055e43
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-28 15:46:20 +0530

    using NOT like operator

--- Table/logical_operators.sql
+++ Table/logical_operators.sql
@@ -1,6 +1,8 @@
 -- using not equal !=
 SELECT title FROM books WHERE released_year!=2013;
 
-
+-- using NOT like
+SELECT title FROM books WHERE title LIKE '% %';
+SELECT title FROM books WHERE title NOT LIKE '% %';
 
 
