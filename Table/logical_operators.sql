fd5815d496dbd1d0354aea5628bcaec41dc83177
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-28 17:21:07 +0530

    used case property

--- Table/logical_operators.sql
+++ Table/logical_operators.sql
@@ -108,7 +108,45 @@ FROM
 WHERE
     released_year >= 2000 AND released_year % 2 != 0;
 
+-- case studies
 
+SELECT 
+    title,
+    released_year,
+    CASE
+        WHEN released_year >= 2000 THEN 'Modern Lit'
+        ELSE '20th Century Lit'
+    END AS Genre
+FROM
+    books;
+
+
+SELECT 
+    title,
+    stock_quantity,
+    CASE
+        WHEN stock_quantity BETWEEN 0 AND 40 THEN '*'
+        WHEN stock_quantity BETWEEN 41 AND 70 THEN '**'
+        WHEN stock_quantity BETWEEN 71 AND 100 THEN '***'
+        WHEN stock_quantity BETWEEN 101 AND 140 THEN '****'
+        ELSE '*****'
+    END AS stock
+FROM
+    books;
+ 
+ 
+SELECT 
+    title,
+    stock_quantity,
+    CASE
+        WHEN stock_quantity <= 40 THEN '*'
+        WHEN stock_quantity <= 70 THEN '**'
+        WHEN stock_quantity <= 100 THEN '***'
+        WHEN stock_quantity <= 140 THEN '****'
+        ELSE '*****'
+    END AS stock
+FROM
+    books;
 
 
 
