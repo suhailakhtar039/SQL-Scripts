a2b0c5296f15f616fdd589794539ae0fe475a057
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-28 16:17:46 +0530

    logical and implemented

--- Table/logical_operators.sql
+++ Table/logical_operators.sql
@@ -23,6 +23,20 @@ SELECT * FROM books WHERE released_year <= 1985;
 
 -- logical and &&
 SELECT * FROM books WHERE author_lname = 'Eggers' && released_year > 2010;
-SELECT * FROM books WHERE author_lname = 'Eggers' and released_year > 2010;
+SELECT 
+    *
+FROM
+    books
+WHERE
+    author_lname = 'Eggers'
+        AND released_year > 2010
+        AND title LIKE '%novel%';
+        
+SELECT 
+    title, pages
+FROM
+    books
+WHERE
+    CHAR_LENGTH(title) > 30 AND pages > 500;
 
 
