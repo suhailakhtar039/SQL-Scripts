07511d39a9e0acfa886d8043b3c286ec788977e2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-28 15:59:43 +0530

    comparing to nothingess i.e. NULL

--- Table/logical_operators.sql
+++ Table/logical_operators.sql
@@ -8,4 +8,11 @@ SELECT title FROM books WHERE title NOT LIKE '% %';
 SELECT title FROM books WHERE title LIKE '%e%';
 SELECT title FROM books WHERE title NOT LIKE '%e%';
 
+-- using greater than and smaller than operator
+SELECT * FROM books WHERE released_year > 2010;
+SELECT * FROM books WHERE pages > 500;
+SELECT 1 > NULL;
+SELECT NULL > 1;
+
+
 
