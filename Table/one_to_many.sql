ab4907cf79097e2d3fda6999393cd62286ddbd9f
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-01 15:26:36 +0530

    foreign key added

--- Table/one_to_many.sql
+++ Table/one_to_many.sql
@@ -11,7 +11,8 @@ CREATE TABLE orders (
     id INT PRIMARY KEY AUTO_INCREMENT,
     order_date DATE,
     amount DECIMAL(8 , 2 ),
-    customer_id INT
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES customers(id)
 );
 
 INSERT INTO customers (first_name, last_name, email) 
