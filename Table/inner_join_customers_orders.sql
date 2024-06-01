5a56cfd0cbf4f73cb52f2be70a8a19d22d234419
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-01 16:03:35 +0530

    inner join or join working correctly

--- Table/inner_join_customers_orders.sql
+++ Table/inner_join_customers_orders.sql
@@ -4,3 +4,17 @@ FROM
     customers
         JOIN
     orders ON customers.id = orders.customer_id;
+    
+    
+SELECT 
+    first_name, last_name, order_date, amount
+FROM
+    customers
+        INNER JOIN
+    orders ON customers.id = orders.customer_id;    
+
+
+    
+    
+    
+    
