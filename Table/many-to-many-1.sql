7dd6b7a12336023801cbe34cd2d56ac29fa68d35
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-03 13:12:44 +0530

    exercise completed

--- Table/many-to-many-1.sql
+++ Table/many-to-many-1.sql
@@ -2,6 +2,7 @@ select * from reviews;
 select * from series;
 select * from reviewers;
 
+-- first exercise
 SELECT 
     title, rating
 FROM
@@ -9,8 +10,15 @@ FROM
         JOIN
     reviews ON series.id = reviews.series_id;
 
-
-
+-- second
+SELECT 
+    title, AVG(rating) AS avg_rating
+FROM
+    series
+        JOIN
+    reviews ON reviews.series_id = series.id
+GROUP BY title
+ORDER BY avg_rating;
 
 
 
