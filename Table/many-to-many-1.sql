0f55f1309a358a7242ae766a46c23975236dbbba
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-03 16:28:52 +0530

    second approach for the same

--- Table/many-to-many-1.sql
+++ Table/many-to-many-1.sql
@@ -37,6 +37,15 @@ FROM
     reviews ON series.id = reviews.series_id
 WHERE
     rating IS NULL;
+    
+SELECT 
+    title AS unreviewed_series
+FROM
+    reviews
+        RIGHT JOIN
+    series ON series.id = reviews.series_id
+WHERE
+    rating IS NULL;
 
 
 
