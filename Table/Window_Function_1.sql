8b2d52e67f849f2d6db264a3363100f92016197d
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-05 16:15:13 +0530

    rank for department salary

--- Table/Window_Function_1.sql
+++ Table/Window_Function_1.sql
@@ -66,8 +66,11 @@ SELECT
 	emp_no,
     department,
     salary,
+    RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_rank,
     RANK() OVER(ORDER BY salary DESC) AS overall_salary_rank
-FROM employees;
+FROM employees ORDER BY department;
+
+
 
 
 
