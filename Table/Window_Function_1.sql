e117726c87cf8196bc73f8c35b9d3fe1fc0616d1
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-05 16:19:39 +0530

    row number calculated

--- Table/Window_Function_1.sql
+++ Table/Window_Function_1.sql
@@ -70,7 +70,15 @@ SELECT
     RANK() OVER(ORDER BY salary DESC) AS overall_salary_rank
 FROM employees ORDER BY department;
 
-
+-- row number
+SELECT
+	emp_no,
+    department,
+    salary,
+    ROW_NUMBER() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_row_number,
+    RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_rank,
+    RANK() OVER(ORDER BY salary DESC) AS overall_salary_rank
+FROM employees ORDER BY department;
 
 
 
