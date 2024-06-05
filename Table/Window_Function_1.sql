48e0993268bf372b99b33ed06f50b3022e634ff3
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-05 15:45:12 +0530

    finding sum of each department

--- Table/Window_Function_1.sql
+++ Table/Window_Function_1.sql
@@ -17,6 +17,29 @@ SELECT AVG(salary) OVER() FROM employees;
 select emp_no, department, salary, avg(salary) over() from employees;
 select emp_no, department, salary, min(salary) over(), max(salary) over() from employees;
 
+-- using partition by
+SELECT 
+	emp_no, department, salary, AVG(salary) OVER(PARTITION BY department) AS dept_avg
+FROM employees;
+
+SELECT 
+	emp_no, department, salary,
+    AVG(salary) OVER(PARTITION BY department) AS dept_avg,
+    AVG(salary) OVER() AS company_avg
+FROM employees;
+
+-- finding no of people in each department
+SELECT
+	department, count(*) OVER(PARTITION BY department) AS no_of_people
+FROM employees;
+
+-- finding sum of salary in each department
+SELECT
+	emp_no, department, salary, SUM(salary) OVER(PARTITION BY department) AS sum_department
+FROM employees;
+
+
+
 
 
 
