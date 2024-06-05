386a7094bb29635af57cb52a30f8259cf8065fd6
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-05 16:23:20 +0530

    dense rank added as well

--- Table/Window_Function_1.sql
+++ Table/Window_Function_1.sql
@@ -80,7 +80,17 @@ SELECT
     RANK() OVER(ORDER BY salary DESC) AS overall_salary_rank
 FROM employees ORDER BY department;
 
-
+-- dense rank
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    ROW_NUMBER() OVER(PARTITION BY department ORDER BY SALARY DESC) as dept_row_number,
+    RANK() OVER(PARTITION BY department ORDER BY SALARY DESC) as dept_salary_rank,
+    RANK() OVER(ORDER BY salary DESC) as overall_rank,
+    DENSE_RANK() OVER(ORDER BY salary DESC) as overall_dense_rank,
+    ROW_NUMBER() OVER(ORDER BY salary DESC) as overall_num
+FROM employees ORDER BY overall_rank;
 
 
 
