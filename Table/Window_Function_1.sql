309ed34d03c3de046a8e1a939afa79fb34d16311
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-05 16:38:41 +0530

    first value function completed

--- Table/Window_Function_1.sql
+++ Table/Window_Function_1.sql
@@ -101,7 +101,21 @@ SELECT
 	NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
 FROM employees;
 
+-- FIRST value
+SELECT
+	emp_no,
+    department,
+    salary,
+    FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC)
+FROM employees;
 
+SELECT
+	emp_no,
+    department,
+    salary,
+    FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) AS highest_paid_dept,
+    FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) AS highest_paid_overall
+FROM employees;
 
 
 
