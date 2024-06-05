ccf58717afd3b88e0b903fca2eebe38a5cf223ad
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-05 15:34:44 +0530

    used partition by in over

--- Table/Window_Function_1.sql
+++ Table/Window_Function_1.sql
@@ -14,6 +14,17 @@ GROUP BY department;
 -- the over clause constructs a window. when it's empty the window will include all the reports
 SELECT AVG(salary) OVER() FROM employees;
 
+select emp_no, department, salary, avg(salary) over() from employees;
+select emp_no, department, salary, min(salary) over(), max(salary) over() from employees;
+
+
+
+
+
+
+
+
+
 
 
 
