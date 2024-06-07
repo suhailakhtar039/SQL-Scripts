d345ed8270e8af797df32352134dea08f783256c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-07 17:00:14 +0530

    user schema created

---
+++ Instagram_clone/ig_clone.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ig_clone;
+USE ig_clone;
+
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
