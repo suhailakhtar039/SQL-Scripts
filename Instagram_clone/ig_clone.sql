7dd87a25308bf11e5f0496f5c387223c47b83366
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-07 18:18:53 +0530

    follows table created

--- Instagram_clone/ig_clone.sql
+++ Instagram_clone/ig_clone.sql
@@ -41,5 +41,17 @@ CREATE TABLE likes (
     FOREIGN KEY (user_id)
         REFERENCES users (id),
     FOREIGN KEY (photo_id)
-        REFERENCES photos (id)
+        REFERENCES photos (id),
+    PRIMARY KEY (user_id , photo_id)
+);
+
+CREATE TABLE follows (
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (follower_id)
+        REFERENCES users (id),
+    FOREIGN KEY (followee_id)
+        REFERENCES userd (id),
+	PRIMARY KEY(follower_id, followee_id)
 );
