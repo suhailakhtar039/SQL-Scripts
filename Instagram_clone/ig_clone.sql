81aed2dd8c11117f0e8e72ca5e437c38271e8e6c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-07 17:58:58 +0530

    comments table created

--- Instagram_clone/ig_clone.sql
+++ Instagram_clone/ig_clone.sql
@@ -22,4 +22,14 @@ CREATE TABLE photos (
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-DESC photos;
+CREATE TABLE comments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (photo_id)
+        REFERENCES photos (id)
+);
