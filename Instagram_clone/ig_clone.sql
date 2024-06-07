cbeadbb24a573ab3b483d028fec26b3d5fffea42
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-07 18:04:30 +0530

    likes table created

--- Instagram_clone/ig_clone.sql
+++ Instagram_clone/ig_clone.sql
@@ -33,3 +33,13 @@ CREATE TABLE comments (
     FOREIGN KEY (photo_id)
         REFERENCES photos (id)
 );
+
+CREATE TABLE likes (
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (photo_id)
+        REFERENCES photos (id)
+);
