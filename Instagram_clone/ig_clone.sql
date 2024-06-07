1c87c23a08555e76807c3919302320640e2826c2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-07 17:12:12 +0530

    photos schema created

--- Instagram_clone/ig_clone.sql
+++ Instagram_clone/ig_clone.sql
@@ -13,3 +13,13 @@ INSERT INTO users(username) VALUES
 ('ColtSteele');
 
 SELECT * FROM users;
+
+CREATE TABLE photos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+DESC photos;
