bdf716784f5857a7c16ce533fec2f9795c26951c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-08 13:03:11 +0530

    what day is most popular for uploading

--- Instagram_clone/exercise.sql
+++ Instagram_clone/exercise.sql
@@ -3,5 +3,14 @@ SELECT
     username
 FROM
     users
-ORDER BY id
+ORDER BY created_at
 LIMIT 5;
+
+-- 2 what day of week do most user register on
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(DAYNAME(created_at)) AS cnt_day
+FROM
+    users
+GROUP BY DAYNAME(created_at)
+ORDER BY cnt_day DESC;
