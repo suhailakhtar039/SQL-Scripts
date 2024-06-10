d2e89299f131bc26c2bde011494ca07b1a782afc
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-10 23:37:18 +0530

    add an alternate solution as well

--- Instagram_clone/exercise.sql
+++ Instagram_clone/exercise.sql
@@ -61,3 +61,13 @@ GROUP BY tag_name
 ORDER BY cnt DESC
 LIMIT 5;
 
+-- solution provided
+SELECT tags.tag_name, 
+       Count(*) AS total 
+FROM   photo_tags 
+       JOIN tags 
+         ON photo_tags.tag_id = tags.id 
+GROUP  BY tags.id 
+ORDER  BY total DESC 
+LIMIT  5; 
+
