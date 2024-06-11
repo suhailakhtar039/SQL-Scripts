4e603c38eca84af085a0262a35f5d5f5f4b27b53
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-11 23:42:27 +0530

    values entered into table users

--- node_example/schema.sql
+++ node_example/schema.sql
@@ -3,3 +3,8 @@ CREATE TABLE users
     email VARCHAR(255) PRIMARY KEY,
     created_at TIMESTAMP DEFAULT NOW()
 );
+
+INSERT INTO users
+    (email)
+VALUES('suhail@test.com'),
+    ('akhtar@test.com');
