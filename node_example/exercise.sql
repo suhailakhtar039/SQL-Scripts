80cb06f7f9a8a2c10e8957f344420f9e28d3177d
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-13 22:51:51 +0530

    extracting month name

--- node_example/exercise.sql
+++ node_example/exercise.sql
@@ -5,3 +5,7 @@ FROM users
 WHERE created_at = (SELECT MIN(created_at)
 FROM users);
 
+-- extracting month user joined
+select MONTHNAME(created_at) as month, count(MONTHNAME(created_at))
+from users
+group by month;
