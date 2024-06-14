f1a6218de9e8b1f8887ac3b809e426cfbb134214
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-14 23:49:40 +0530

    mysql integrated

--- webApp/app.js
+++ webApp/app.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql2');
+ 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Suh@il786',
+  database: 'check_with_node'
+});
 
 app.get('/', (req, res) =>{
     res.send("HELLO FROM OUR WEB APP");
