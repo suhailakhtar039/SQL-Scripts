3cc5ba353f43a4e4b02741a8d347b9c26951594b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-14 23:37:13 +0530

    joke about dogs

--- webApp/app.js
+++ webApp/app.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) =>{
     res.send("HELLO FROM OUR WEB APP");
 })
 
+app.get('/joke', (req, res) =>{
+    res.send('SIMPLE JOKE SEE ABOUT DOG');
+    console.log("see me in the console");
+})
+
 app.listen(8080, () =>{
     console.log('listening')
 })
